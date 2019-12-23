3971e0a7e2dc1233b7acbfab3b620384895670e6
ZamanidisAlexios <sdi1600047@di.uoa.gr>
2019-12-23 02:13:06 +0200

    add wall-clock execution time

--- source/main.c
+++ source/main.c
@@ -16,9 +16,13 @@ void test_function(void *argument) {
 }
 
 int main(void) {
+    struct timespec begin, end;
+    double time_spent;
 
     job_scheduler = initialize_job_scheduler(NUMBER_OF_THREADS);
     create_threads_job_scheduler();
+
+    clock_gettime(CLOCK_MONOTONIC, &begin);
 
     for( int i = 0 ; i < 10 ; i ++) {
         struct test *test = (struct test *)malloc(sizeof(struct test));
@@ -28,6 +32,12 @@ int main(void) {
     }
 
     barrier_job_scheduler();
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    time_spent = (end.tv_sec - begin.tv_sec);
+    time_spent = time_spent + (end.tv_nsec-begin.tv_nsec)/1000000000.0;
+    printf("Execution time = %f\n",time_spent);
+
     stop_job_scheduler();
     free_job_scheduler();
 
